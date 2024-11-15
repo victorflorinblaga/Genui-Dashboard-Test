ecafa8c9fc5da091b555ed2cf4a55f88ca4f30fe
victorflorinblaga <115701667+victorflorinblaga@users.noreply.github.com>
2024-11-15 10:45:21 +0100

    updating contents of template/src/app/dashboards/othertest@gmail.com/ca8aaf20-5d6a-4ed8-b481-290f36b24c40/page.tsx

--- template/src/app/dashboards/othertest@gmail.com/ca8aaf20-5d6a-4ed8-b481-290f36b24c40/page.tsx
+++ template/src/app/dashboards/othertest@gmail.com/ca8aaf20-5d6a-4ed8-b481-290f36b24c40/page.tsx
@@ -1,18 +1,19 @@
 
   import react from "react";
-  import { panel0 } from "./components/panel0";
-  import { panel1 } from "./components/panel1";
-  import { panel2 } from "./components/panel2";
-  import { panel3 } from "./components/panel3";
+  import Panel0 from "./components/panel0";
+  import Panel1 from "./components/panel1";
+  import Panel2 from "./components/panel2";
+  import Panel3 from "./components/panel3";
   
   export default function Dashboard() {
     return (
       <div>
         <h1>Dashboard</h1>
-        <panel0 />
-        <panel1 />
-        <panel2 />
-        <panel3 />
+        <Panel0 />
+        <Panel1 />
+        <Panel2 />
+        <Panel3 />
       </div>
     );
+}
       
