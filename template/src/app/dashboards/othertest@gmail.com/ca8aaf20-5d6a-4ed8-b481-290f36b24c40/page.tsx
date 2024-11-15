6e4e773afcb13def914bbb6d5f4c9d48ee1b4f1f
victorflorinblaga <115701667+victorflorinblaga@users.noreply.github.com>
2024-11-15 10:33:04 +0100

    creating file: template/src/app/dashboards/othertest@gmail.com/ca8aaf20-5d6a-4ed8-b481-290f36b24c40/page.tsx

---
+++ template/src/app/dashboards/othertest@gmail.com/ca8aaf20-5d6a-4ed8-b481-290f36b24c40/page.tsx
@@ -0,0 +1,18 @@
+
+  import react from "react";
+  import { panel0 } from "./components/panel0";
+  import { panel1 } from "./components/panel1";
+  import { panel2 } from "./components/panel2";
+  import { panel3 } from "./components/panel3";
+  
+  export default function Dashboard() {
+    return (
+      <div>
+        <h1>Dashboard</h1>
+        <panel0 />
+        <panel1 />
+        <panel2 />
+        <panel3 />
+      </div>
+    );
+      
