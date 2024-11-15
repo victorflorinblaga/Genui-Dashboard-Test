be94f2f6e6eb7acb99cc4a7d90f5e58cdcdc36aa
victorflorinblaga <115701667+victorflorinblaga@users.noreply.github.com>
2024-11-15 09:20:25 +0100

    updating contents of template/src/app/dashboards/othertest@gmail.com/ede1d196-7f98-49ed-a0cf-8945a1407def/page.tsx

--- template/src/app/dashboards/othertest@gmail.com/ede1d196-7f98-49ed-a0cf-8945a1407def/page.tsx
+++ template/src/app/dashboards/othertest@gmail.com/ede1d196-7f98-49ed-a0cf-8945a1407def/page.tsx
@@ -1,1 +1,14 @@
-test content
+
+  import react from "react";
+  import { panel0 } from "./components/panel0";
+  import { panel1 } from "./components/panel1";
+  import { panel2 } from "./components/panel2";
+  import { panel3 } from "./components/panel3";
+  
+  export default function Dashboard() {
+    return (
+      <div>
+        <h1>Dashboard</h1>
+      </div>
+    );
+      
