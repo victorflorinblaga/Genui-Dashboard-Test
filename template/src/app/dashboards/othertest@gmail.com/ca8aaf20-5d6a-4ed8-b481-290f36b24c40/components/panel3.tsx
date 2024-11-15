af54fda3aee56e5da75c35033d6a7b09e0ef90e4
victorflorinblaga <115701667+victorflorinblaga@users.noreply.github.com>
2024-11-15 10:45:25 +0100

    updating contents of template/src/app/dashboards/othertest@gmail.com/ca8aaf20-5d6a-4ed8-b481-290f36b24c40/components/panel3.tsx

--- template/src/app/dashboards/othertest@gmail.com/ca8aaf20-5d6a-4ed8-b481-290f36b24c40/components/panel3.tsx
+++ template/src/app/dashboards/othertest@gmail.com/ca8aaf20-5d6a-4ed8-b481-290f36b24c40/components/panel3.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
 import { HoverCard, HoverCardContent, HoverCardTrigger } from "@/components/ui/hover-card";
 
-const panel3 = () => {
+const Panel3 = () => {
   return (
     <div className="relative bg-blue-900 text-white h-screen">
       <div className="absolute inset-0 flex items-center justify-center">
@@ -51,4 +51,4 @@ const panel3 = () => {
   );
 };
 
-export default panel3;
+export default Panel3;
